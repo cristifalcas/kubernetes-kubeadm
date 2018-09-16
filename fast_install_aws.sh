76e3690727a6f0129c9be812bee378c05470882d
Cristian Falcas <cristi.falcas@gmail.com>
2018-09-16 09:11:06 +0300

    Update fast_install_aws.sh

--- fast_install_aws.sh
+++ fast_install_aws.sh
@@ -3,6 +3,63 @@ KUBE_VER=1.9.10
 
 yum install -y aws kubectl-$KUBE_VER kubeadm-$KUBE_VER kubelet-$KUBE_VER
 
+cat <<EOF > /etc/kubernetes/manifests/etcd.yaml 
+apiVersion: v1
+kind: Pod
+metadata:
+  name: etcd-tls
+  namespace: kube-system
+spec:
+  hostNetwork: true
+  containers:
+  - name: etcd
+    command:
+    - etcd
+    - --name=$(NODE_NAME)
+    - --data-dir=/var/lib/etcd/
+    - --wal-dir=/var/lib/etcd/wal/
+    - --listen-peer-urls=https://0.0.0.0:7001
+    - --listen-client-urls=https://0.0.0.0:2379
+    - --advertise-client-urls=https://$(NODE_NAME):2379
+    - --initial-advertise-peer-urls=https://$(NODE_NAME):7001
+    - --initial-cluster=$(NODE_NAME)=https://$(NODE_NAME):7001
+    - --initial-cluster-state=new
+    - --client-cert-auth=true
+    - --peer-client-cert-auth=true
+    - --peer-auto-tls=true
+    - --trusted-ca-file=/etc/kubernetes/pki/ca.crt
+    - --cert-file=/etc/kubernetes/pki/apiserver.crt
+    - --key-file=/etc/kubernetes/pki/apiserver.key
+    image: gcr.io/google_containers/etcd-amd64:3.0.17
+    env:
+    - name: NODE_NAME
+      valueFrom:
+        fieldRef:
+          fieldPath: spec.nodeName
+    livenessProbe:
+      failureThreshold: 8
+      tcpSocket:
+        port: 2379
+      initialDelaySeconds: 15
+      timeoutSeconds: 15
+    volumeMounts:
+    - mountPath: /var/lib/etcd
+      name: etcd
+    - mountPath: /etc/kubernetes/
+      name: k8s
+      readOnly: true
+  securityContext:
+    seLinuxOptions:
+      type: spc_t
+  volumes:
+  - hostPath:
+      path: /var/lib/etcd
+    name: etcd
+  - hostPath:
+      path: /etc/kubernetes
+    name: k8s
+EOF
+
 cat <<EOF > kubeadmin_conf.yaml
 apiVersion: kubeadm.k8s.io/v1alpha1
 kind: MasterConfiguration
